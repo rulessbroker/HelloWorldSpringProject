b181ddb1d5f19a00bba81966c0fd98223c7184d9
rulessbroker <rulessbroker>
2023-01-20 11:18:39 +0530

    Added: Web Rest page Controller

---
+++ src/main/java/com/FirstSpringApp/Controller/HelloWebRestController.java
@@ -0,0 +1,12 @@
+package com.FirstSpringApp.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWebRestController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello From BridgeLabz!!!!!!!!!";
+	}
+}
